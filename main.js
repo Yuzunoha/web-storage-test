c6b5b7d81ad58eeac7cacfc4b1eeadba6961c865
Yuzunoha <studysteadystandby@gmail.com>
2019-12-20 20:40:54 +0900

    ボタンを押してjsの関数を呼び出せた

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 const mainTextArea = document.getElementById('main-text-area');
+const text1 = document.getElementById('text1');
 
 mainTextArea.innerHTML = 'jsから書きました';
+
+// onclick
+const save = () => {
+  alert('save()');
+}
